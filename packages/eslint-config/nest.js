385083a15a11394483361edec62ddf8315e0c6d9
Aleksey Golovin <alexey.golovin@lumiktech.com>
2025-09-29 16:06:00 +0300

    eslint-shared

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -1,10 +1,9 @@
-// @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export const nestjsConfig = [
   {
     ignores: ['eslint.config.js'],
   },
@@ -52,4 +51,4 @@ export default tseslint.config(
       ],
     },
   },
-);
+];
